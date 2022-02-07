51363709d6b3e22a212697cf8eaaca8a08db7077
ckaynatividad <natividad.ckay@gmail.com>
2022-02-07 14:47:13 -0800

    added contexts and entries docs

---
+++ src/context/UserContext.jsx
@@ -0,0 +1,21 @@
+import { createContext, useContext, useMemo } from 'react';
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({});
+
+  const value = useMemo(() => ({ user, setUser }), [user]);
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
+const useUser = () => {
+  const context = useContext(UserContext);
+
+  if (context === undefined) {
+    throw new Error('must be used within userprovider');
+  }
+  return context;
+};
+
+export { UserProvider, useUser };
