01183682b74442c3c207526e49dad9675dd8ed50
ckaynatividad <natividad.ckay@gmail.com>
2022-02-09 23:08:42 -0800

    added dark mode

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,15 +1,29 @@
+import { useState } from 'react';
+import { ThemeProvider } from 'styled-components';
 import Entries from '../components/Entries';
 import GuestBook from '../components/GuestBook';
 import { Header } from '../components/Header';
+import { GlobalStyles } from '../components/Theme/globalStyles';
+import { darkTheme, lightTheme } from '../components/Theme/Themes';
 import { EntryProvider } from '../context/EntryContext';
 //import { EntryProvider } from '../context/EntryContext';
 
 export const Home = () => {
+  const [theme, setTheme] = useState('light');
+  const themeToggler = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light');
+  };
   return (
-    <EntryProvider>
-      <Header />
-      <GuestBook />
-      <Entries />
-    </EntryProvider>
+    <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <GlobalStyles />
+      <div className="main">
+        <button onClick={themeToggler}>Switch</button>
+        <EntryProvider>
+          <Header />
+          <GuestBook />
+          <Entries />
+        </EntryProvider>
+      </div>
+    </ThemeProvider>
   );
 };
