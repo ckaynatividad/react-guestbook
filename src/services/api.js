3d8d2f1d68d9b4f399bf26fb8fe6422997334d40
ckaynatividad <natividad.ckay@gmail.com>
2022-02-11 12:09:17 -0800

    made key private

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
+console.log(process.env.REACT_APP_API_KEY);
 export async function fetchApi() {
   const resp = await fetch('https://api.pexels.com/v1/search?query=wedding', {
     headers: {
-      Authorization: '563492ad6f9170000100000127ccfbbf167b44a6aca36ac91d39b1a6',
+      Authorization: process.env.REACT_APP_API_KEY,
     },
   });
   const data = await resp.json();
